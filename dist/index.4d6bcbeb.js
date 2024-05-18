649cac3c78c267bb7f7f6fffd5c3d36a6e11622b
seung365 <bdh3659@naver.com>
2024-05-19 08:17:16 +0900

    feat: 영화 더보기 버튼 추가

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -749,20 +749,23 @@ var _search = require("../components/Search");
 var _searchDefault = parcelHelpers.interopDefault(_search);
 var _movieList = require("../components/MovieList");
 var _movieListDefault = parcelHelpers.interopDefault(_movieList);
+var _movieListMore = require("../components/MovieListMore");
+var _movieListMoreDefault = parcelHelpers.interopDefault(_movieListMore);
 class Home extends (0, _heropy.Component) {
     render() {
         const headline = new (0, _headlineDefault.default)().el // 이렇게 메인페이지에 append를 통해 밀어넣을것..
         ;
         const search = new (0, _searchDefault.default)().el;
         const movieList = new (0, _movieListDefault.default)().el;
+        const movieListMore = new (0, _movieListMoreDefault.default)().el;
         // 이렇게 하면 home 컴포넌트는 제일 먼저 container라는 클래스 가지는 div요소로 만들어진다.
         this.el.classList.add("container");
-        this.el.append(headline, search, movieList);
+        this.el.append(headline, search, movieList, movieListMore);
     }
 }
 exports.default = Home;
 
-},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","../components/Headline":"gaVgo","../components/Search":"jqPPz","../components/MovieList":"8UDl3"}],"gaVgo":[function(require,module,exports) {
+},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","../components/Headline":"gaVgo","../components/Search":"jqPPz","../components/MovieList":"8UDl3","../components/MovieListMore":"3ZUar"}],"gaVgo":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 var _heropy = require("../core/heropy");
@@ -825,23 +828,23 @@ var _heropy = require("../core/heropy");
 const store = new (0, _heropy.Store)({
     searchText: "",
     page: 1,
+    pageMax: 1,
     movies: []
 });
 exports.default = store;
 const searchMovies = async (page)=>{
-    if (page === 1) {
-        store.state.page = 1;
-        store.state.movies = [];
-    }
+    store.state.page = page;
+    if (page === 1) store.state.movies = [];
     const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`) //s랑 page는 파라미터이다,
     ;
-    const { Search } = await res.json() // 구조 분해 할당
+    const { Search, totalResults } = await res.json() // 구조 분해 할당
     ;
     store.state.movies = [
         ...store.state.movies,
         ...Search
     ] // 이렇게 하면 movies에 데이터 정보가 갱신된다.
     ;
+    store.state.pageMax = Math.ceil(Number(totalResults) / 10);
 };
 
 },{"../components/Search":"jqPPz","../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"8UDl3":[function(require,module,exports) {
@@ -904,6 +907,34 @@ class MovieItem extends (0, _heropy.Component) {
 }
 exports.default = MovieItem;
 
-},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["anvqh","gLLPy"], "gLLPy", "parcelRequire6588")
+},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"3ZUar":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+var _heropy = require("../core/heropy");
+var _movie = require("../store/movie");
+var _movieDefault = parcelHelpers.interopDefault(_movie);
+class MoveListMore extends (0, _heropy.Component) {
+    constructor(){
+        super({
+            tagName: "button"
+        });
+        (0, _movieDefault.default).subscribe("pageMax", ()=>{
+            // movieStore.state.page
+            // movieStore.state.pageMax
+            const { page, pageMax } = (0, _movieDefault.default).state;
+            pageMax > page ? this.el.classList.remove("hide") : this.el.classList.add("hide");
+        });
+    }
+    render() {
+        this.el.classList.add("btn", "view-more", "hide");
+        this.el.textContent = "View more..";
+        this.el.addEventListener("click", async ()=>{
+            await (0, _movie.searchMovies)((0, _movieDefault.default).state.page + 1);
+        });
+    }
+}
+exports.default = MoveListMore;
+
+},{"../core/heropy":"57bZf","../store/movie":"kq1bo","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["anvqh","gLLPy"], "gLLPy", "parcelRequire6588")
 
 //# sourceMappingURL=index.4d6bcbeb.js.map
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,18 +2,21 @@ import { Component } from "../core/heropy";
 import Headline from "../components/Headline";
 import Search from "../components/Search";
 import MovieList from "../components/MovieList";
+import MovieListMore from "../components/MovieListMore";
 
 export default class Home extends Component {
   render() {
     const headline = new Headline().el   // 이렇게 메인페이지에 append를 통해 밀어넣을것..
     const search = new Search().el
     const movieList = new MovieList().el
+    const movieListMore = new MovieListMore().el
     // 이렇게 하면 home 컴포넌트는 제일 먼저 container라는 클래스 가지는 div요소로 만들어진다.
     this.el.classList.add('container')
     this.el.append(
       headline,
       search,
-      movieList
+      movieList,
+      movieListMore
     )
 
   }
