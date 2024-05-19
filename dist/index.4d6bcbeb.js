d79cd4562b12b0b8bbb7534309ba57f3e6611fc1
seung365 <bdh3659@naver.com>
2024-05-20 00:21:16 +0900

    feat: 예외 페이지 접근 에러 처리

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -845,6 +845,8 @@ var _movie = require("./Movie");
 var _movieDefault = parcelHelpers.interopDefault(_movie);
 var _about = require("./About");
 var _aboutDefault = parcelHelpers.interopDefault(_about);
+var _notFound = require("./NotFound");
+var _notFoundDefault = parcelHelpers.interopDefault(_notFound);
 //route란 일반적으로 페이지를 말함
 exports.default = (0, _heropy.createRouter)([
     {
@@ -858,10 +860,14 @@ exports.default = (0, _heropy.createRouter)([
     {
         path: "#/about",
         component: (0, _aboutDefault.default)
+    },
+    {
+        path: ".*",
+        component: (0, _notFoundDefault.default)
     }
 ]);
 
-},{"../core/heropy":"57bZf","./Home":"0JSNG","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./Movie":"1LTyN","./About":"gdB30"}],"0JSNG":[function(require,module,exports) {
+},{"../core/heropy":"57bZf","./Home":"0JSNG","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./Movie":"1LTyN","./About":"gdB30","./NotFound":"4fDiL"}],"0JSNG":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 var _heropy = require("../core/heropy");
@@ -1184,6 +1190,23 @@ class About extends (0, _heropy.Component) {
 }
 exports.default = About;
 
-},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","../store/about":"4RAJO"}]},["anvqh","gLLPy"], "gLLPy", "parcelRequire6588")
+},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","../store/about":"4RAJO"}],"4fDiL":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+var _heropy = require("../core/heropy");
+class NotFound extends (0, _heropy.Component) {
+    render() {
+        this.el.classList.add("container", "not-found");
+        this.el.innerHTML = /* html */ `
+      <h1>
+        Sorry..<br>
+        Page Not Found.
+      </h1>
+    `;
+    }
+}
+exports.default = NotFound;
+
+},{"../core/heropy":"57bZf","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["anvqh","gLLPy"], "gLLPy", "parcelRequire6588")
 
 //# sourceMappingURL=index.4d6bcbeb.js.map
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ import { createRouter } from '../core/heropy'
 import Home from './Home'
 import Movie from './Movie'
 import About from './About'
+import NotFound from './NotFound'
 //route란 일반적으로 페이지를 말함
 
 
 export default createRouter([
   { path: '#/', component: Home },
   { path: '#/movie', component: Movie },
-  { path: '#/about', component: About }
-  
+  { path: '#/about', component: About },
+  { path: '.*', component: NotFound }
+
+  // 순서대로 적어줘야 한다. 왜냐하면 위에서 매치가 되면 밑에껀 보지 않기 때문이다.
 ])
