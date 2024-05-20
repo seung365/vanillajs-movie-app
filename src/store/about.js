06e69fc851641ec8a1cd48b21bc5168ec2b6b66b
seung365 <bdh3659@naver.com>
2024-05-20 21:22:59 +0900

    feat: about.js photo url 변경

--- src/store/about.js
+++ src/store/about.js
@@ -1,7 +1,7 @@
 import { Store } from "../core/heropy";
 
 export default new Store({
-  photo: 'https://avatars.githubusercontent.com/u/74394824?s=96&v=4',
+  photo: 'https://github.com/seung365/vanillajs-movie-app/blob/main/src/img/icon.png?raw=true',
   name: 'seung365 / BaekSeungBeom',
   email: 'bdh3659@naver.com',
   velog: 'https://velog.io/@seung365/posts',
