29032f9d512e7ed3cf2e5298251417c716f74c79
seung365 <bdh3659@naver.com>
2024-05-25 21:45:53 +0900

    Chore: icon 이미지 변경-1

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -3,27 +3,27 @@ import { Component } from "../core/heropy";
 export default class TheHeader extends Component {
   constructor() {
     super({
-      tagName: 'header',
+      tagName: "header",
       state: {
         menus: [
           {
-            name: 'Search',
-            href: '#/'
+            name: "Search",
+            href: "#/",
           },
           {
-            name: 'Movie',
-            href: '#/movie?id=tt4520988'
+            name: "Movie",
+            href: "#/movie?id=tt4520988",
           },
           {
-            name: 'About',
-            href: '#/about'
-          }
-        ]
-      }
-    })
-    window.addEventListener('popstate', () => {
-      this.render()
-    })
+            name: "About",
+            href: "#/about",
+          },
+        ],
+      },
+    });
+    window.addEventListener("popstate", () => {
+      this.render();
+    });
   }
   render() {
     this.el.innerHTML = /* html */ `
@@ -32,24 +32,26 @@ export default class TheHeader extends Component {
       </a>
       <nav>
         <ul>
-          ${this.state.menus.map(menu => {
-            const href = menu.href.split('?')[0]
-            const hash = location.hash.split('?')[0] 
-            const isActive = href === hash  // 해당하는 메뉴의 링크 주소와 현재 페이지의 실제 주소를 비교 
-            return /* html */ `
+          ${this.state.menus
+            .map((menu) => {
+              const href = menu.href.split("?")[0];
+              const hash = location.hash.split("?")[0];
+              const isActive = href === hash; // 해당하는 메뉴의 링크 주소와 현재 페이지의 실제 주소를 비교
+              return /* html */ `
               <li>
-                <a class = "${isActive ? 'active' : ''}"
+                <a class = "${isActive ? "active" : ""}"
                 href="${menu.href}">
                 ${menu.name}
                 </a>
               </li>
-            `
-          }).join('')}
+            `;
+            })
+            .join("")}
         </ul>
       </nav>
       <a href="#/about" class="user">
-        <img src="https://avatars.githubusercontent.com/u/74394824?s=96&v=4" alt="user">
+        <img src="https://github.com/seung365/vanillajs-movie-app/blob/main/src/img/icon.png?raw=true" alt="user">
       </a>
-    `
+    `;
   }
 }
